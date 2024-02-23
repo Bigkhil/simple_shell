ccaff279ce7bf35333ed07a1b47ae02e0b8d503d
Mohamed khalil <khm40005000@outlook.com>
2024-02-23 02:48:13 +0200

    some changes

--- Functions_1.c
+++ Functions_1.c
@@ -1,30 +1,51 @@
 #include "main.h"
-
+/**
+ * print_k - print
+ * @format: string
+ * Return: void
+ */
 void print_k(const char *format)
 {
-    write(STDOUT_FILENO,format,strlen(format));
+	write(STDOUT_FILENO, format, strlen(format));
 }
-
+/**
+ * split_it - tokenize the string
+ *@str: string
+ * Return: array of tokens
+ */
 char **split_it(char *str)
 {
-    char *token;
-    char **tokens = NULL;
-    int i = 0;                
+	char *token = NULL, *str_copy = NULL;
+	char **tokens = NULL;
+	int num_tokens = 0, i;
 
-    token = strtok(str," \t\n");
-    while(token != NULL)
-    {
-        i++;
-        tokens = realloc(tokens, i * sizeof(char *));
-        if (tokens == NULL)
-        {
-            perror("realloc");
-            exit(EXIT_FAILURE);
-        }
-        tokens[i - 1] = token;
-
-        token = strtok(NULL," \n\t");
-    }
-    tokens[i] = NULL;
-    return tokens;
+	str_copy = malloc(sizeof(char) * strlen(str));
+	strcpy(str_copy, str);
+	token = strtok(str, " \n");
+	while (token != NULL)
+	{
+		num_tokens++;
+		token = strtok(NULL, " \n");
+	}
+	tokens = malloc(sizeof(char *) * num_tokens);
+	if (tokens == NULL)
+	{
+		perror("couldn't allocate memory for tokens");
+		exit(-1);
+	}
+	token = NULL;
+	token = strtok(str_copy, " \n");
+	for (i = 0 ; i < num_tokens ; i++)
+	{
+		tokens[i] = malloc(sizeof(char) * strlen(token));
+		if (tokens[i] == NULL)
+		{
+			perror("couldn't allocate memory for tokens[i]");
+			exit(-1);
+		}
+		strcpy(tokens[i], token);
+		token = strtok(NULL, " \n");
+	}
+	free(str_copy);
+	return (tokens);
 }
--- main.c
+++ main.c
@@ -1,26 +1,25 @@
 #include "main.h"
 /**
  * main - check the code
- *@argc: number of arguments
- *@argv: pointer to array of strings
+ * @argc: number of arguments
+ * @argv: pointer to array of strings
+ * @env: pointer to array of strings
  * Return: Always 0.
  */
 int main(int argc, char *argv[], char *env[])
 {
 
-	char *prompt = "karim & khalil Shell$ ", *buff;
+	char *prompt = "karim & khalil Shell$ ", *buff = NULL;
 	int i = 0;
 	char **tokens = NULL;
 	size_t buff_size = 0;
 	ssize_t ret_getline;
 
-
 	while (1)
 	{
+		buff = NULL;
 		print_k(prompt);
 		ret_getline = getline(&buff, &buff_size, stdin);
-
-
 		if (ret_getline == -1)
 		{
 			free(buff);
@@ -28,9 +27,9 @@ int main(int argc, char *argv[], char *env[])
 		}
 		buff[ret_getline - 1] = '\0';
 		tokens = split_it(buff);
-		execute_cmd(tokens[0],tokens,NULL);
-		
+		execute_cmd(tokens[0], tokens);
 		free(tokens);
+		free(buff);
 	}
-    return (0);
+	return (0);
 }
