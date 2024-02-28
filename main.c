6bbb56aef3177d7a052c780a780e2c704a139038
Mohamed khalil <khm40005000@outlook.com>
2024-02-28 17:49:23 +0200

    some changes

--- main.c
+++ main.c
@@ -5,36 +5,33 @@
  * @argv: pointer to array of strings
  * Return: Always 0.
  */
-int main(int argc, char *argv[], char *env[])
+int main(int argc, char *argv[])
 {
 	char *prompt = "karim&khalil$ ", *buff = NULL, *cmd_path;
 	char **tokens;
 	size_t buff_size = 0, i = 0;
 	ssize_t ret_getline;
-	(void)argc, (void)argv, (void)env;
+
+	(void)argc, (void)argv;
 	while (1)
 	{
 		i = 0;
 		buff = NULL, tokens = NULL, cmd_path = NULL;
+		if (isatty(STDIN_FILENO))
+			print_k(prompt);
 		ret_getline = getline(&buff, &buff_size, stdin);
 		if (ret_getline == -1)
 		{
-			print_k("\n");
 			free(buff);
 			exit(EXIT_FAILURE);
 		}
 		buff[ret_getline - 1] = '\0';
 		tokens = split_it(buff);
-		if (strcmp(tokens[0], "exit") == 0)
-		{
-			exit(0);
-			free(tokens);
-		}
-		else
-		{
 		cmd_path = get_path(tokens[0]);
 		if (cmd_path != NULL)
+		{
 			execute_cmd(cmd_path, tokens);
+		}
 		else
 			execute_cmd(tokens[0], tokens);
 		while (tokens[i] != NULL)
@@ -44,7 +41,6 @@ int main(int argc, char *argv[], char *env[])
 		}
 		free(tokens[i]);
 		free(tokens), free(buff), free(cmd_path);
-		}
 	}
 	return (0);
 }
