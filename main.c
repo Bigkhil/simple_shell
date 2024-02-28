14172d4aed45bb5937f0e3972b56d77bf035bcea
karim-mohamedd <karimmohamed5555555@gmail.com>
2024-02-28 22:14:17 +0200

    new update

--- main.c
+++ main.c
@@ -5,47 +5,38 @@
  * @argv: pointer to array of strings
  * Return: Always 0.
  */
-int main(int argc, char *argv[], char *env[])
+int main(int argc, char *argv[])
 {
 	char *prompt = "karim&khalil$ ", *buff = NULL, *cmd_path;
 	char **tokens;
-	size_t buff_size = 0, i = 0;
+	size_t buff_size = 0;
 	ssize_t ret_getline;
-	(void)argc, (void)argv, (void)env;
+
+	(void)argc, (void)argv;
 	while (1)
 	{
-		i = 0;
-		buff = NULL, tokens = NULL, cmd_path = NULL;
-		print_k(prompt);
+		if (isatty(STDIN_FILENO))
+			print_k(prompt);
 		ret_getline = getline(&buff, &buff_size, stdin);
-		if (ret_getline == -1)
+		if (ret_getline == -1 || strcmp(buff, "exit\n") == 0)
 		{
-			print_k("\n");
 			free(buff);
-			exit(EXIT_FAILURE);
+			exit(ret_getline == -1 ? EXIT_FAILURE : EXIT_SUCCESS);
 		}
 		buff[ret_getline - 1] = '\0';
 		tokens = split_it(buff);
-		if (strcmp(tokens[0], "exit") == 0)
+		if (tokens)
 		{
-			exit(0);
-			free(tokens);
+			cmd_path = get_path(tokens[0]);
+			execute_cmd(cmd_path ? cmd_path : tokens[0], tokens);
+			for (int i = 0; tokens[i] != NULL; i++)
+			{
+				free(tokens[i]);
+			}
+			free(tokens), free(cmd_path);
 		}
-		else
-		{
-		cmd_path = get_path(tokens[0]);
-		if (cmd_path != NULL)
-			execute_cmd(cmd_path, tokens);
-		else
-			execute_cmd(tokens[0], tokens);
-		while (tokens[i] != NULL)
-		{
-			free(tokens[i]);
-			i++;
-		}
-		free(tokens[i]);
-		free(tokens), free(buff), free(cmd_path);
-		}
+		free(buff);
+		buff = NULL, buff_size = 0;
 	}
 	return (0);
 }
